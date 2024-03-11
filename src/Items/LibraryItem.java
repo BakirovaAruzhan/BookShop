15576cbb6ab7a4102f0c15d2a731d59678b2038f
Bakirova Aruzhan <bakirovaauzhan@gmail.com>
2024-03-11 13:54:20 +0500

    работа с ошибками

--- src/Items/LibraryItem.java
+++ src/Items/LibraryItem.java
@@ -1,8 +1,6 @@
 package Items;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 
 public abstract class LibraryItem {
 
@@ -49,15 +47,63 @@ public abstract class LibraryItem {
     public static ArrayList<DVD> dvds = new ArrayList<>();
     public static ArrayList<Magazine> magazines = new ArrayList<>();
 
+    public static ArrayList<Book> getBooks() {
+        return books;
+    }
 
-    public static void initData() {
+    public static ArrayList<DVD> getDVDs() {
+        return dvds;
+    }
 
-
-
-
+    public static ArrayList<Magazine> getMagazines() {
+        return magazines;
     }
 
 
+    public static ArrayList<LibraryItem> getItems() {
+        ArrayList<LibraryItem> items = new ArrayList<>();
+        items.addAll(books);
+        items.addAll(dvds);
+        items.addAll(magazines);
+        return items;
+    }
+
+    public void borrowItem(int itemId, String userLogin) {
+        ArrayList<LibraryItem> items = getItems();
+
+        if (itemId >= 0 && itemId < items.size()) {
+            LibraryItem item = items.get(itemId);
+            if (item.isAvailable()) {
+                item.setAvailable(false);
+                System.out.println("Элемент взят в аренду пользователем " + userLogin + ": " + item.getName());
+            } else {
+                System.out.println("Элемент недоступен для аренды.");
+            }
+        } else {
+            System.out.println("Некорректный номер элемента.");
+        }
+    }
+
+
+    public void returnItem(int itemId, String userLogin) {
+        ArrayList<LibraryItem> items = getItems();
+
+        if (itemId >= 0 && itemId < items.size()) {
+            LibraryItem item = items.get(itemId);
+            if (!item.isAvailable()) {
+                item.setAvailable(true);
+                System.out.println("Элемент возвращен пользователем " + userLogin + ": " + item.getName());
+            } else {
+                System.out.println("Элемент уже доступен.");
+            }
+        } else {
+            System.out.println("Некорректный номер элемента.");
+        }
+    }
+
+    public static void initData() {
+
+    }
 }
 
 
